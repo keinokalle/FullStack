e773fc5cb53a9b91496c8b78a30a8a9a19dea905
keinokalle <keinokalle@gmail.com>
2025-06-10 12:29:28 +0300

    finished exercises 1.1 to 1.5

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -1,52 +1,64 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const [ counter, setCounter ] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  setTimeout(() => {
+    () => setCounter(counter + 1)
+  }, 1000);
 
   return (
     <div>
+      <h1>Counter {counter}</h1>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course} />
+      <Total parts={course} />
     </div>
   )
 }
-
 
 export default App
 
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course.name}</h1>
 
 
 const Content = ({ parts }) => {
+  console.log(parts);
   return (
     <div>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
+      <Part part={parts.parts[0].name} exercises={parts.parts[0].exercises} />
+      <Part part={parts.parts[1].name} exercises={parts.parts[1].exercises} />
+      <Part part={parts.parts[2].name} exercises={parts.parts[2].exercises} />
     </div>
   )
 }
 
-const Total = ({ parts }) => (
+const Total = ({ parts }) => {
+  console.log(parts);
   <p>
-    Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    Number of exercises {parts.parts[0].exercises + parts.parts[1].exercises + parts.parts[2].exercises}
   </p>
-)
+}
 
 const Part = ({ part, exercises }) => {
+  console.log(part, exercises);
   return (
     <p>
       {part} {exercises}
