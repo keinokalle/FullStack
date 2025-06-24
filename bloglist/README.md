d0deed182de41a1a489e1e4f2cc520521b1f49eb
keinokalle <keinokalle@gmail.com>
2025-06-24 18:40:04 +0300

    exercises until 4.2

---
+++ bloglist/README.md
@@ -0,0 +1,1 @@
+Blog posting application, course work
---
+++ bloglist/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use('', blogsRouter)
+
+module.exports = app
